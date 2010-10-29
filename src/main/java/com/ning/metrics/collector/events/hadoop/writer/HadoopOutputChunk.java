a7af237a3639141ac5526ab5711a52a3bb434af2
Pierre-Alexandre Meyer <pierre@ning.com>
2010-10-29 14:00:48 -0700

    writer: hadoop: re-indent HadoopOutputChunk
    Fix also error message to be more helpful.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- src/main/java/com/ning/metrics/collector/events/hadoop/writer/HadoopOutputChunk.java
+++ src/main/java/com/ning/metrics/collector/events/hadoop/writer/HadoopOutputChunk.java
@@ -16,69 +16,68 @@
 
 package com.ning.metrics.collector.events.hadoop.writer;
 
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
 
 import java.io.IOException;
 
-//package local
 class HadoopOutputChunk
 {
-	private final Path sourcePath;
-	private final Path desintationPath;
-	private final SequenceFile.Writer writer;
-	private boolean isClosed = false;
+    private final Path sourcePath;
+    private final Path desintationPath;
+    private final SequenceFile.Writer writer;
+    private boolean isClosed = false;
 
-	HadoopOutputChunk(Path sourcePath, Path desintationPath, SequenceFile.Writer writer)
-	{
-		this.sourcePath = sourcePath;
-		this.desintationPath = desintationPath;
-		this.writer = writer;
-	}
+    HadoopOutputChunk(Path sourcePath, Path desintationPath, SequenceFile.Writer writer)
+    {
+        this.sourcePath = sourcePath;
+        this.desintationPath = desintationPath;
+        this.writer = writer;
+    }
 
-	public void close() throws IOException
-	{
-		if (!isClosed) {
-			isClosed = true;
-			writer.close();
-		}
-	}
+    public void close() throws IOException
+    {
+        if (!isClosed) {
+            isClosed = true;
+            writer.close();
+        }
+    }
 
-	public void commit(FileSystem fileSystem) throws IOException
-	{
-		Path destinationDir = desintationPath.getParent();
+    public void commit(FileSystem fileSystem) throws IOException
+    {
+        Path destinationDir = desintationPath.getParent();
 
-		// parent directory has to exist for a hdfs rename to succeed
-		if (!fileSystem.exists(destinationDir) && !fileSystem.mkdirs(desintationPath.getParent())) {
-			throw new IOException(String.format("unable to make destination directory %s", destinationDir));
-		}
-		if (!fileSystem.rename(sourcePath, desintationPath)) {
-			throw new IOException(String.format("unable to rename %s to %s", sourcePath, desintationPath));
-		}
-	}
+        // parent directory has to exist for a hdfs rename to succeed
+        if (!fileSystem.exists(destinationDir) && !fileSystem.mkdirs(desintationPath.getParent())) {
+            throw new IOException(String.format("Unable to make destination directory %s (does the parent directory exist?)", destinationDir));
+        }
+        if (!fileSystem.rename(sourcePath, desintationPath)) {
+            throw new IOException(String.format("Unable to rename %s to %s", sourcePath, desintationPath));
+        }
+    }
 
-	public void rollback(FileSystem fileSystem) throws IOException
-	{
-		deleteIfExists(sourcePath, fileSystem);
-		deleteIfExists(desintationPath, fileSystem);
-	}
+    public void rollback(FileSystem fileSystem) throws IOException
+    {
+        deleteIfExists(sourcePath, fileSystem);
+        deleteIfExists(desintationPath, fileSystem);
+    }
 
-	private void deleteIfExists(Path path, FileSystem fileSystem) throws IOException
-	{
-		if (fileSystem.exists(path) && !fileSystem.delete(path, false)) {
-			throw new IOException(String.format("unable to delete %s", path));
-		}
-	}
+    private void deleteIfExists(Path path, FileSystem fileSystem) throws IOException
+    {
+        if (fileSystem.exists(path) && !fileSystem.delete(path, false)) {
+            throw new IOException(String.format("unable to delete %s", path));
+        }
+    }
 
-	public SequenceFile.Writer getWriter()
-	{
-		return writer;
-	}
+    public SequenceFile.Writer getWriter()
+    {
+        return writer;
+    }
 
-	@Override
-	public String toString()
-	{
-		return String.format("%s : %s -> %s", HadoopOutputChunk.class, sourcePath, desintationPath);
-	}
+    @Override
+    public String toString()
+    {
+        return String.format("%s : %s -> %s", HadoopOutputChunk.class, sourcePath, desintationPath);
+    }
 }
