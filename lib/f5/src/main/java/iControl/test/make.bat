28cf01df38e3eb3542d8425dca53e8fec3ab2bf2
Pierre-Alexandre Meyer <pierre@ning.com>
2010-12-15 07:46:09 -0800

    f5: create sources jar
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- lib/f5/src/main/java/iControl/test/make.bat
+++
@@ -1,5 +0,0 @@
-Set JAVALIB=D:\src\perforce\DevCentral\DC4\Labs\iControlAssembly\java\3rdparty\jar\Axis1
-set AXIS_CLASSPATH=%JAVALIB%\axis.jar;%JAVALIB%\commons-discovery.jar;%JAVALIB%\commons-logging.jar;%JAVALIB%\jaxrpc.jar;%JAVALIB%\junit.jar;%JAVALIB%\saaj.jar;%JAVALIB%\wsdl4j.jar;%JAVALIB%\activation.jar;%JAVALIB%\mail.jar
-:c:\jdk1.6.0_16\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar SystemInfo.java
-c:\jdk1.6.0_16\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar PoolMember.java
-:c:\jdk1.6.0_16\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar Main.java
