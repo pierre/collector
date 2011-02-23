e96e6113f5d15cb8bdeaf3c301cbe122b2d3e769
Pierre-Alexandre Meyer <pierre@ning.com>
2011-02-23 15:40:08 -0800

    util: initial import of imap2coll
    This script forwards events from an IMAP mailbox to Scribe.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ src/utils/py/imap2coll.py
@@ -0,0 +1,125 @@
+#
+# Copyright 2011 Ning, Inc.
+#
+# Ning licenses this file to you under the Apache License, version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Simple script to test the Thrift endpoint of the collector
+#
+
+import argparse
+import base64
+import imaplib
+import logging
+import sys
+
+from scribe import scribe
+from thrift.transport import TTransport, TSocket
+from thrift.protocol import TBinaryProtocol
+
+from opsAlert.ttypes import OpsAlert
+
+
+log = logging.getLogger('imap2coll')
+
+def get_messages(host, user, password):
+    """Download all messages (e.g. pages) from the mailbox
+
+    Keyword arguments:
+    host -- IMAP server
+    user -- login connection information
+    password -- password connection information
+
+    """
+    M = imaplib.IMAP4()
+    M.login(IMAP_USER, IMAP_PASS)
+
+    M.select()
+    typ, data = M.search(None, 'ALL')
+    messages = []
+    for num in data[0].split():
+        typ, data = M.fetch(num, '(RFC822)')
+        print 'Message %s\n%s\n' % (num, data[0][1])
+    M.close()
+
+    M.logout()
+
+    log.info('Found %d messages at %s', len(messages), host)
+    return messages
+
+
+def create_event(src, msgFrom, msgTo, msgSubject, msgDate):
+    """Create Thrift event from a message
+
+    Keyword arguments:
+    src -- unique identifier for the source of the page (e.g. Nagios, Pages, ...)
+    msgFrom -- email FROM header
+    msgTo -- email TO header
+    msgSubject -- email subject
+    msgDate -- time when the message was received
+
+    """
+    transportOut = TTransport.TMemoryBuffer()
+    protocolOut = TBinaryProtocol.TBinaryProtocol(transportOut)
+
+    timeInMilliSinceEpoch = int(msgDate) * 1000
+
+    opsAlert = OpsAlert(src, msgFrom, msgTo, msgSubject, timeInMilliSinceEpoch)
+    opsAlert.write(protocolOut)
+    opsAlertInBytes = transportOut.getvalue()
+    log.debug('Created: %s', str(opsAlert))
+
+    return scribe.LogEntry("OpsAlert", str(timeInMilliSinceEpoch) + ":" + base64.b64encode(opsAlertInBytes))
+
+
+def send_events(events, scribeHost, scribePort):
+    """Send all specified events
+
+    Keyword arguments:
+    events -- list of LogEntry events to send
+    scribeHost -- Scribe server to talk to
+    scribePort -- Scribe server's port
+
+    """
+    socket = TSocket.TSocket(host=scribeHost, port=scribePort)
+    transport = TTransport.TFramedTransport(socket)
+    protocol = TBinaryProtocol.TBinaryProtocol(trans=transport, strictRead=False, strictWrite=False)
+    client = scribe.Client(iprot=protocol, oprot=protocol)
+
+    transport.open()
+    log.info('Sending %d messages to %s:%d', len(events), scribeHost, scribePort)
+    result = client.Log(messages=[logEntry])
+    transport.close()
+
+    return result
+
+def main(argv=None):
+    handler = logging.StreamHandler(sys.stdout)
+    log.setLevel(logging.DEBUG)
+    log.addHandler(handler)
+    log.debug('Logging handler configured')
+
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(description='Get pages from an IMAP mailbox and forward them remotely to Scribe.')
+    parser.add_argument('--imapServer', required=True, help='IMAP server to connect to')
+    parser.add_argument('--imapUsername', required=True, help='IMAP username')
+    parser.add_argument('--imapPassword', required=True, help='IMAP password')
+    parser.add_argument('--scribeHost', required=True, help='Scribe server to connect to')
+    parser.add_argument('--scribePort', required=True, help='Scribe port')
+    args = parser.parse_args()
+
+    get_messages(args.imapServer, args.imapUsername, args.imapPassword)
+
+if __name__ == "__main__":
+    sys.exit(main())
