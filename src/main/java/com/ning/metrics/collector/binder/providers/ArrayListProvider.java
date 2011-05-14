968d33e2c81a7c523810dc7d6916ac103afe30d7
Pierre-Alexandre Meyer <pierre@ning.com>
2011-05-13 20:17:58 -0700

    guice: remove unused code in ArrayListProvider
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- src/main/java/com/ning/metrics/collector/binder/providers/ArrayListProvider.java
+++ src/main/java/com/ning/metrics/collector/binder/providers/ArrayListProvider.java
@@ -23,29 +23,18 @@ import com.google.inject.Provider;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ArrayListProvider<T> implements Provider<List<T>>
 {
     private Injector injector;
-    private final List<Key<? extends T>> injectables = new ArrayList<Key<? extends T>>();
+    private final Collection<Key<? extends T>> injectables = new ArrayList<Key<? extends T>>();
 
     @Inject
     public void configure(final Injector injector)
     {
         this.injector = injector;
-    }
-
-    public ArrayListProvider<T> add(final Class<? extends T> toBeIncluded)
-    {
-        injectables.add(Key.get(toBeIncluded));
-        return this;
-    }
-
-    public ArrayListProvider<T> add(final Class<? extends Annotation> annotation, final Class<? extends T> toBeIncluded)
-    {
-        injectables.add(Key.get(toBeIncluded, annotation));
-        return this;
     }
 
     public ArrayListProvider<T> add(final Annotation annotation, final Class<? extends T> toBeIncluded)
