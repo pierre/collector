5430d493ce0bb67ae7dc537bc4249f196061c295
Cole Ott <cole@ning.com>
2011-06-03 11:09:17 -0700

    JettyTestModule: changed delay to 1 second (from 0 seconds)
    * less than optimal to wait a whole second, but '0 seconds' was
      causing an IllegalArgumentException

--- src/test/java/com/ning/metrics/collector/endpoint/resources/JettyTestModule.java
+++ src/test/java/com/ning/metrics/collector/endpoint/resources/JettyTestModule.java
@@ -86,6 +86,6 @@ public class JettyTestModule extends AbstractModule
         final MockEventWriter writer = new MockEventWriter();
         bind(MockEventWriter.class).toInstance(writer);
         // Wrap around ThresholdEventWriter to trigger commits
-        bind(PersistentWriterFactory.class).toInstance(new MockPersistentWriterFactory(new ThresholdEventWriter(writer, 0, 0)));
+        bind(PersistentWriterFactory.class).toInstance(new MockPersistentWriterFactory(new ThresholdEventWriter(writer, 0, 1)));
     }
 }
